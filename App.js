f123bb31e984257f5aa61476780feadfe4e908c6
Hanna <hannamlittlefield@gmail.com>
2022-08-02 09:26:43 -0400

    adding Imagebackground to App

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
+import React from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground} from 'react-native';
+
+const backgroundImage = require('./Images/blackVelvet.png')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={backgroundImage} resizeMode="cover" style={styles.image}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </ImageBackground>
     </View>
   );
 }
