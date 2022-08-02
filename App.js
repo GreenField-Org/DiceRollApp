6cecfb465d72b4be0975c3d532397e68fbacc1be
Hanna <hannamlittlefield@gmail.com>
2022-08-02 09:28:00 -0400

    styling ImageBackground to cover whole screen

--- App.js
+++ App.js
@@ -8,7 +8,6 @@ export default function App() {
   return (
     <View style={styles.container}>
       <ImageBackground source={backgroundImage} resizeMode="cover" style={styles.image}>
-        <Text>Open up App.js to start working on your app!</Text>
       </ImageBackground>
     </View>
   );
@@ -17,8 +16,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center"
   },
 });
